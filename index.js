4ce3682e7daf258a065fb7dd8bc2ffc912575090
venkyroy22 <venkateswarakonetisetty@gmail.com>
2025-01-31 10:18:51 +0530

    done

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
-const { resolve } = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
-const port = 3010;
+const PORT = process.env.PORT || 5000;
+require('dotenv').config(); 
+const MONGO_URI = process.env.MONGO_URI;
 
-app.use(express.static('static'));
+
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log('Connected to database'))
+  .catch((err) => console.error('Error connecting to database:', err));
+
 
 app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+  res.send('Customer Management System Backend is Running');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
